39e289074fb9b6bce6d336108e65aaf060a5ba20
minji1110 <kalswl991110@gmail.com>
2021-11-03 20:21:07 +0900

    echo with select

--- echo_with_select/select_client.c
+++ echo_with_select/select_client.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 void error_handling(char *message);
 void str_cli(FILE *fp, int sockfd);
-#define BUF_SIZE 1024
+#define BUF_SIZE 10
 
 int main(int argc, char *argv[]){
     int sockfd;
@@ -15,7 +20,7 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
-	if(sockfd=socket(AF_INET,SOCK_STREAM,0)<0)
+	if((sockfd=socket(AF_INET,SOCK_STREAM,0))<0)
 		error_handling("socket() error");
 	
 	bzero(&serv_addr,sizeof(serv_addr));
@@ -26,7 +31,12 @@ int main(int argc, char *argv[]){
 	if(connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1)
 		error_handling("connect() error");
         
-    else str_cli(stdin, sockfd);
+    else{
+        // int fd=open("text.txt",O_CREAT|O_RDONLY);
+        // if(fd==-1) error_handling("file open() error");
+        // str_cli(fd, sockfd);
+        str_cli(stdin, sockfd);
+    }    
     
     exit(0);
 }
@@ -42,12 +52,12 @@ void str_cli(FILE *fp, int sockfd){
     while(1){
         if(stdin_eof==0) FD_SET(fileno(fp), &read_set);
         FD_SET(sockfd, &read_set);
-        max_fd=max(fileno(fp),sockfd)+1;
+        max_fd=fmax(fileno(fp),sockfd)+1;
         
         if(select(max_fd, &read_set, NULL, NULL, NULL)<0) break;
 
         if(FD_ISSET(sockfd, &read_set)){
-            if(n=read(sockfd, buf, BUF_SIZE)==0){   //FIN 받음
+            if((n=read(sockfd, buf, BUF_SIZE))==0){   //FIN 받음
                 if(stdin_eof==1) return;    //내가 이미 shutdown 한 상태에서 FIN을 받은 이 경우에만 최종 종료!
                 else error_handling("모든 데이터를 보내기 전 서버로부터 FIN을 받음");
             }
@@ -56,7 +66,7 @@ void str_cli(FILE *fp, int sockfd){
         }
 
         if(FD_ISSET(fileno(fp),&read_set)){
-            if(n=read(fileno(fp),buf,BUF_SIZE)==0){ //ctrl+D 입력
+            if((n=read(fileno(fp),buf,BUF_SIZE))==0){ //ctrl+D 입력
                 stdin_eof=1;
                 shutdown(sockfd, SHUT_WR);
                 FD_CLR(sockfd, &read_set);
--- echo_with_select/select_server.c
+++ echo_with_select/select_server.c
@@ -1,20 +1,24 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-#define BUF_SIZE 100
+#define BUF_SIZE 10
 void error_handling(char *msg);
 
 int main(int argc, char const *argv[])
 {
-    int listen_fd, conn_fd,clnt_len;
+    int listen_fd, conn_fd;
     struct sockaddr_in serv_addr, clnt_addr;   //<arpa/inet.h>
+    socklen_t clnt_len;
     fd_set read_set, temp_set;
     int max_fd, max_conn, nready;
     int client[FD_SETSIZE];
     char buf[BUF_SIZE];
 
-    if(listen_fd=socket(AF_INET,SOCK_STREAM,0)<0)
+    if((listen_fd=socket(AF_INET,SOCK_STREAM,0))<0)
         error_handling("socket() error");
     
     bzero(&serv_addr,sizeof(serv_addr));
@@ -39,10 +43,11 @@ int main(int argc, char const *argv[])
 
     while(1){
         temp_set=read_set;
-        if(nready=select(max_fd+1, &temp_set, NULL, NULL, NULL)<0) break;
+        if((nready=select(max_fd+1, &temp_set, NULL, NULL, NULL))<0) break;
 
         //readable한 fd가 리스닝 소켓
         if(FD_ISSET(listen_fd,&temp_set)){
+            printf("select return : listening socket, nready=%d\n",nready);
             clnt_len=sizeof(clnt_addr);
             conn_fd=accept(listen_fd,(struct sockaddr*) &clnt_addr, &clnt_len);
             
@@ -57,7 +62,7 @@ int main(int argc, char const *argv[])
             }
             
             FD_SET(conn_fd,&read_set);
-            if(conn_fd>max_fd) max_conn = conn_fd;
+            if(conn_fd>max_fd) max_fd = conn_fd;
             printf("connect! fd= %d\n",conn_fd);
 
             //listening 소켓이 끝이면 connect 소켓들은 볼 필요 없음, 다시 select 대기 상태로
@@ -67,12 +72,11 @@ int main(int argc, char const *argv[])
 
         int readable_fd,n;
         //readable 한 fd가 client 소켓 중 존재
-        for(int i=0;i<max_conn;i++){
-
+        for(int i=0;i<=max_conn;i++){
             if((readable_fd=client[i])<0) continue;
 
             if(FD_ISSET(readable_fd,&temp_set)){
-                if(n=read(readable_fd, buf, BUF_SIZE)==0){
+                if((n=read(readable_fd, buf, BUF_SIZE))==0){
                     FD_CLR(readable_fd,&read_set);
                     close(readable_fd);
                     client[i]=-1;
@@ -80,6 +84,8 @@ int main(int argc, char const *argv[])
                 }
                 else{
                     write(readable_fd, buf, n);
+                    fputc('\n',stdout);
+                    sleep(3);
                 }
 
                 //더이상 readable한 소켓 없는경우 뒷부분은 안봐도 됨, 다시 select 대기 상태로
